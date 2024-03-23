b34669ba968de750db6060fc7a0808bf0bc12a6d
Md Rahman <rahmanmd954@gmail.com>
2024-03-23 13:35:50 -0500

    added map

--- gesture_library.js
+++ gesture_library.js
@@ -1,37 +1,34 @@
 class GestureLibrary {
     constructor(cameraCapture) {
       this.cameraCapture = cameraCapture;
-      this.handEmojis = new Set([
-        '👋', // Waving Hand
-        '✋', // Raised Hand
-        '👌', // OK Hand
-        '✌️', // Victory Hand
-        '🤞', // Crossed Fingers
-        '🤟', // Love-You Gesture
-        '🤘', // Sign of the Horns
-        '🤙', // Call Me Hand
-        '👈', // Backhand Index Pointing Left
-        '👉', // Backhand Index Pointing Right
-        '👆', // Backhand Index Pointing Up
-        '👇', // Backhand Index Pointing Down
-        '☝️', // Index Pointing Up
-        '👍', // Thumbs Up
-        '👎', // Thumbs Down
-        '✊', // Raised Fist
-        '👊', // Oncoming Fist
-        '🤛', // Left-Facing Fist
-        '🤜', // Right-Facing Fist
-        '🤚', // Raised Back of Hand
-        '👋', // Waving Hand
-        '🤏', // Pinching Hand
-        '👏', // Clapping Hands
-        '👐', // Open Hands
-        '🙌', // Raising Hands
-        '🤲', // Palms Up Together
-        '🙏', // Folded Hands
-        '✍️', // Writing Hand
-        '💪', // Flexed Biceps
-      ]);
+      this.handEmojisMap = new Map([
+        ['Waving Hand', '👋'],
+        ['Open Hand', '✋'],
+        ['OK Hand', '👌'],
+        ['Peace', '✌️'],
+        ['Crossed Fingers', '🤞'],
+        ['Love-You Gesture', '🤟'],
+        ['Sign of the Horns', '🤘'],
+        ['Call Me Hand', '🤙'],
+        ['Backhand Index Pointing Left', '👈'],
+        ['Backhand Index Pointing Right', '👉'],
+        ['Backhand Index Pointing Up', '👆'],
+        ['Backhand Index Pointing Down', '👇'],
+        ['Index Pointing Up', '☝️'],
+        ['Thumbs Up', '👍'],
+        ['Thumbs Down', '👎'],
+        ['Raised Fist', '✊'],
+        ['Closed Fist', '👊'],
+        ['Open Hand', '🤚'],
+        ['Pinching Hand', '🤏'],
+        ['Clapping Hands', '👏'],
+        ['Open Hands', '👐'],
+        ['Raising Hands', '🙌'],
+        ['Palms Up Together', '🤲'],
+        ['Folded Hands', '🙏'],
+        ['Writing Hand', '✍️'],
+        ['Flexed Biceps', '💪']
+    ]);
     }
   
     // Add your existing methods here...
