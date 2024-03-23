6bff3eb14f521be318b8efb37c0a49d5a2acec56
Md Rahman <rahmanmd954@gmail.com>
2024-03-23 10:44:20 -0500

    style updates

--- gesture_library.js
+++ gesture_library.js
@@ -1,1 +1,38 @@
-
+class GestureLibrary {
+    constructor(cameraCapture) {
+      this.cameraCapture = cameraCapture;
+      this.handEmojis = new Set([
+        '👋', // Waving Hand
+        '✋', // Raised Hand
+        '👌', // OK Hand
+        '✌️', // Victory Hand
+        '🤞', // Crossed Fingers
+        '🤟', // Love-You Gesture
+        '🤘', // Sign of the Horns
+        '🤙', // Call Me Hand
+        '👈', // Backhand Index Pointing Left
+        '👉', // Backhand Index Pointing Right
+        '👆', // Backhand Index Pointing Up
+        '👇', // Backhand Index Pointing Down
+        '☝️', // Index Pointing Up
+        '👍', // Thumbs Up
+        '👎', // Thumbs Down
+        '✊', // Raised Fist
+        '👊', // Oncoming Fist
+        '🤛', // Left-Facing Fist
+        '🤜', // Right-Facing Fist
+        '🤚', // Raised Back of Hand
+        '👋', // Waving Hand
+        '🤏', // Pinching Hand
+        '👏', // Clapping Hands
+        '👐', // Open Hands
+        '🙌', // Raising Hands
+        '🤲', // Palms Up Together
+        '🙏', // Folded Hands
+        '✍️', // Writing Hand
+        '💪', // Flexed Biceps
+      ]);
+    }
+  
+    // Add your existing methods here...
+  }
